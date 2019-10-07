3f0679e2d92ea603a0cd31ef6634640f29776297
sunkw <sunkwei@gmail.com>
2019-10-07 17:22:05 +0800

    天涯论坛 增加几个站点

--- tianya.py
+++ tianya.py
@@ -10,58 +10,79 @@ _sites = [
     {
         "title": "天涯杂谈",
         "baseurl": "http://bbs.tianya.cn/list-free-1.shtml",
-        "pattern": r"/post-free-\d+-1.shtml",
+        "pattern": r"/post-free-\d+-1\.shtml",
     },
     {
         "title": "新闻众评",
         "baseurl": "http://bbs.tianya.cn/list-news-1.shtml",
-        "pattern": r"/post-news-\d+-1.shtml",
+        "pattern": r"/post-news-\d+-1\.shtml",
     },
     {
         "title": "我的大学",
         "baseurl": "http://bbs.tianya.cn/list-university-1.shtml",
-        "pattern": r"/post-university-\d+-1.shtml",
+        "pattern": r"/post-university-\d+-1\.shtml",
     },
     {
         "title": "关天茶馆",
         "baseurl": "http://bbs.tianya.cn/list-no01-1.shtml",
-        "pattern": r"/post-no01-\d+-1.shtml",
+        "pattern": r"/post-no01-\d+-1\.shtml",
     },
     {
         "title": "闲闲书话",
         "baseurl": "http://bbs.tianya.cn/list-books-1.shtml",
-        "pattern": r"/post-books-\d+-1.shtml",
+        "pattern": r"/post-books-\d+-1\.shtml",
     },
     {
         "title": "国际观察",
         "baseurl": "http://bbs.tianya.cn/list-worldlook-1.shtml",
-        "pattern": r"/post-worldlook-\d+-1.shtml",
+        "pattern": r"/post-worldlook-\d+-1\.shtml",
     },
     {
         "title": "心灵热线",
         "baseurl": "http://bbs.tianya.cn/list-spirit-1.shtml",
-        "pattern": r"/post-spirit-\d+-1.shtml",
+        "pattern": r"/post-spirit-\d+-1\.shtml",
     },
     {
         "title": "学术中国",
         "baseurl": "http://bbs.tianya.cn/list-666-1.shtml",
-        "pattern": r"/post-666-\d+-1.shtml",
+        "pattern": r"/post-666-\d+-1\.shtml",
     },
     {
         "title": "人物研究",
         "baseurl": "http://bbs.tianya.cn/list-113-1.shtml",
-        "pattern": r"/post-113-\d+-1.shtml",
+        "pattern": r"/post-113-\d+-1\.shtml",
     },
     {
         "title": "语文学习",
         "baseurl": "http://bbs.tianya.cn/list-1170-1.shtml",
-        "pattern": r"/post-1170-\d+-1.shtml",
+        "pattern": r"/post-1170-\d+-1\.shtml",
     },
     {
         "title": "文学批评",
         "baseurl": "http://bbs.tianya.cn/list-187-1.shtml",
-        "pattern": r"/post-187-\d+-1.shtml",
+        "pattern": r"/post-187-\d+-1\.shtml",
     },
+    {
+        "title": "煮酒论史",
+        "baseurl": "http://bbs.tianya.cn/list-no05-1.shtml",
+        # http://bbs.tianya.cn/post-no05-507160-1.shtml
+        "pattern": r"/post-no05-\d+-1\.shtml"
+    },
+    {
+        "title": "舞文弄墨",
+        "baseurl": "http://bbs.tianya.cn/list-culture-1.shtml",
+        "pattern": r"/post-culture-\d+-1\.shtml"
+    },
+    {
+        "title": "影视评论",
+        "baseurl": "http://bbs.tianya.cn/list-filmtv-1.shtml",
+        "pattern": r"/post-filmtv-\d+-1\.shtml"
+    },
+    {
+        "title": "法制天地",
+        "baseurl": "http://bbs.tianya.cn/list-law-1.shtml",
+        "pattern": r"/post-law-\d+-1\.shtml"
+    }
 ]
 
 def get(url):
